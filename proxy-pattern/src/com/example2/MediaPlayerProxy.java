8823009fc47c103cc8907480dee3539fca0d9651
planetofthecode <rajkumar.karnakar@outlook.com>
2024-05-02 19:33:50 +0530

    proxy pattern second example done

---
+++ proxy-pattern/src/com/example2/MediaPlayerProxy.java
@@ -0,0 +1,14 @@
+package com.example2;
+
+public class MediaPlayerProxy implements MediaPlayer {
+
+    private MovieMediaPlayer movieMediaPlayer;
+
+    @Override
+    public void playMedia() {
+        if (movieMediaPlayer == null) {
+            movieMediaPlayer = new MovieMediaPlayer();
+        }
+        movieMediaPlayer.playMedia();
+    }
+}
