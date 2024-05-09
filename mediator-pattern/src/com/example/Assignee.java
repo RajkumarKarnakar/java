9e1ea13e07bb828bb6dfa2a3e662da89ae35a39d
planetofthecode <rajkumar.karnakar@outlook.com>
2024-05-09 17:46:52 +0530

    added mediator pattern

---
+++ mediator-pattern/src/com/example/Assignee.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Assignee {
+
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public void pickUpTicket (Ticket ticket){
+        System.out.println("Ticket picked up by assignee");
+        tickets.add(ticket);
+    }
+
+    public void resolveTicket(Ticket ticket){
+        tickets.remove(ticket);
+        System.out.println("Ticket resolved");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
