5365afa9ee0a302f836cdda687504556b8b49f20
planetofthecode <rajkumar.karnakar@outlook.com>
2024-05-11 15:39:29 +0530

    1st example of strategy pattern

---
+++ strategy-pattern/src/com/example/LoginPage.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class LoginPage {
+
+    public void logInUser(Authenticator authenticator){
+        authenticator.logIn();
+    }
+}
---
+++ strategy-pattern/src/com/example/SingleSignOnAuthenticator.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class SingleSignOnAuthenticator implements Authenticator{
+    @Override
+    public void logIn() {
+        System.out.println("Logging in with single sign on!");
+    }
+}
