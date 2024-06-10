1680e264160eab34c8ba8cb0787f04fc3da429bf
planetofthecode <rajkumar.karnakar@outlook.com>
2024-06-10 20:57:46 +0530

    diagonal diffrence is done

---
+++ diagonal-difference/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[][]{{10, 20, 30}, {15, 25, 35}, {1, 2, 3}};
+
+        for (int[] a : matrix) {
+            for (int i : a) {
+                System.out.print(i + ",");
+            }
+            System.out.println();
+        }
+        System.out.println("diagonal difference of matrix is: " + diagonalDifference(matrix));
+
+    }
+
+    private static int diagonalDifference(int[][] arr) {
+        int diff = 0;
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            rightSum += arr[i][i];
+            leftSum += arr[i][arr.length - 1 - i];
+        }
+        diff = rightSum - leftSum;
+        return diff;
+    }
+}
--- simple-array-sum/src/Main.java
+++ simple-array-sum/src/Main.java
@@ -4,20 +4,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.MAX_VALUE);
         System.out.println("please enter the array size");
         int n = sc.nextInt();
-        int[] nums = new int[n];
+        int[] numbs = new int[n];
         System.out.println("enter the array elements");
         for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+            numbs[i] = sc.nextInt();
         }
-        System.out.println("Your array is: "+ Arrays.toString(nums));
-        arraySum(nums);
-
-
+        System.out.println("Your array is: " + Arrays.toString(numbs));
+        arraySum(numbs);
+        bigSumOfArray(numbs);
 
     }
-    private static void arraySum(int[] arr){
-        System.out.println("Sum is: "+Arrays.stream(arr).sum());
+
+    private static void arraySum(int[] arr) {
+        System.out.println("Sum is: " + Arrays.stream(arr).sum());
+    }
+    private static void bigSumOfArray(int[] arr){
+        System.out.println("Sum is: "+Arrays
+                .stream(Arrays.stream(arr).mapToLong(i-> i)
+                        .toArray()).sum());
     }
 }
