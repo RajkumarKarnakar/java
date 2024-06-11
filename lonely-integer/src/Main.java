4a2f67c32d18b04f9a3cb271294ee0cd37e7fc7a
planetofthecode <rajkumar.karnakar@outlook.com>
2024-06-11 08:48:45 +0530

    lonely integer done

--- lonely-integer/src/Main.java
+++ lonely-integer/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 0, 1, 0, 3, 3};
+        int[] arr = new int[]{1,2,3,2,1};
         int result = 0;
         for (int i = 0; i < arr.length; i++) {
             result ^= arr[i];
