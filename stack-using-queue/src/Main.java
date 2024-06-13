541c90ebe00b5b03dd8865d79a6de55d4d9c9866
planetofthecode <rajkumar.karnakar@outlook.com>
2024-06-13 22:01:41 +0530

    stack functions using queue

---
+++ stack-using-queue/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    static class Stack {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        static void push(int x) {
+            q2.add(x);
+            while (!q1.isEmpty()) {
+                q2.add((q1.peek()));
+                q1.remove();
+            }
+            Queue<Integer> q = q1;
+            q1 = q2;
+            q2 = q;
+        }
+
+        static void pop() {
+            if (q1.isEmpty())
+                return;
+            q1.remove();
+        }
+
+        static int top() {
+            if (q1.isEmpty())
+                return -1;
+            return q1.peek();
+        }
+
+        static int size() {
+            return q1.size();
+        }
+
+        static void print() {
+            q1.stream().forEach(i -> System.out.println("-> " + i));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.print();
+        System.out.println();
+        System.out.println("current size: " + s.size());
+        System.out.println("top ->" + s.top());
+        s.pop();
+        System.out.println("top ->" + s.top());
+        s.pop();
+        System.out.println("top ->" + s.top());
+
+        System.out.println("current size: " + s.size());
+    }
+}
